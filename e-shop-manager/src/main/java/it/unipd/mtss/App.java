ecb36db31b5d61641c1a5f5c64510c0bdf7fdc81
LucaBru <lucabrugnera01@gmail.com>
2022-05-14 17:09:58 +0200

    Creato progetto maven

---
+++ e-shop-manager/src/main/java/it/unipd/mtss/App.java
@@ -0,0 +1,18 @@
+////////////////////////////////////////////////////////////////////
+// LUCA BRUGNERA 2014722
+// MATTEO CESCON 2009984
+////////////////////////////////////////////////////////////////////
+
+package it.unipd.mtss;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
